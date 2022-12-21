e39e21876d859f19fd9fd95ca34dd3e8098d3d1f
Francisco Encalada <francisco.encalada01@epn.edu.ec>
2022-12-20 19:00:54 -0500

    Ciclo de vida part 2

--- android_1/app/src/main/java/com/example/android_1/ACicloVidaActividades.kt
+++ android_1/app/src/main/java/com/example/android_1/ACicloVidaActividades.kt
@@ -12,7 +12,8 @@ import com.example.android_1.databinding.ActivityAcicloVidaActividadesBinding
 class ACicloVidaActividades : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-private lateinit var binding: ActivityAcicloVidaActividadesBinding
+    private lateinit var binding: ActivityAcicloVidaActividadesBinding
+    var textoGlobal = "";
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +31,46 @@ private lateinit var binding: ActivityAcicloVidaActividadesBinding
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+        mostrarSnackbar("OnCreate")
     }
-
     override fun onSupportNavigateUp(): Boolean {
     val navController = findNavController(R.id.nav_host_fragment_content_aciclo_vida_actividades)
     return navController.navigateUp(appBarConfiguration)
             || super.onSupportNavigateUp()
     }
+
+    fun mostrarSnackbar(texto:String){
+        textoGlobal += texto
+        Snackbar.make(findViewById(R.id.cl_ciclo_vida),
+            textoGlobal, Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
+    }
+
+    override fun onStart (){
+        super.onStart()
+        mostrarSnackbar("onStart")
+    }
+    override fun onResume(){
+        super.onResume()
+        mostrarSnackbar("onResume")
+    }
+    override fun onRestart (){
+        super.onRestart()
+        mostrarSnackbar("onRestart")
+    }
+    override fun onPause(){
+        super.onPause()
+        mostrarSnackbar("onPause")
+    }
+    override fun onStop(){
+        super.onStop()
+        mostrarSnackbar("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mostrarSnackbar("onDestroy")
+    }
+
+
 }
