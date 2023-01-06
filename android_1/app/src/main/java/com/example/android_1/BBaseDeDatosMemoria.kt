070962c1be65836275b685185db269b242d6dccf
Francisco Encalada <francisco.encalada01@epn.edu.ec>
2023-01-05 22:18:52 -0500

    Clases Kotlin parte 2

--- android_1/app/src/main/java/com/example/android_1/BBaseDeDatosMemoria.kt
+++ android_1/app/src/main/java/com/example/android_1/BBaseDeDatosMemoria.kt
@@ -1,19 +1,21 @@
 package com.example.android_1
 
 class BBaseDeDatosMemoria {
-    val arregloBEntrenador = arrayListOf<BEntrenador>()
-    init {
-        arregloBEntrenador
-            .add(
-                BEntrenador("Francisco", "a@a.com")
-            )
-        arregloBEntrenador
-            .add(
-                BEntrenador("Vicente", "b@b.com")
-            )
-        arregloBEntrenador
-            .add(
-                BEntrenador("Carolina", "c@c.com")
-            )
+    companion object{
+        val arregloBEntrenador = arrayListOf<BEntrenador>()
+        init {
+            arregloBEntrenador
+                .add(
+                    BEntrenador("Francisco", "a@a.com")
+                )
+            arregloBEntrenador
+                .add(
+                    BEntrenador("Vicente", "b@b.com")
+                )
+            arregloBEntrenador
+                .add(
+                    BEntrenador("Carolina", "c@c.com")
+                )
+        }
     }
 }
