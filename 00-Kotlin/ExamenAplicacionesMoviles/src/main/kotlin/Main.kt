42d3d4c7c230dfc5d09728710eeda59d4f2f8880
Francisco Encalada <francisco.encalada01@epn.edu.ec>
2023-01-15 23:18:51 -0500

    Examen AM parte 3

--- 00-Kotlin/ExamenAplicacionesMoviles/src/main/kotlin/Main.kt
+++ 00-Kotlin/ExamenAplicacionesMoviles/src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 import java.time.LocalDate
 import java.util.Scanner
+import java.util.StringJoiner
 
 //Global variables
 val scanner = Scanner(System.`in`)
@@ -36,12 +37,12 @@ data class Band(
 ) : DataFile(firstParameter, secondParameter, thirdParameter, fourthParameter, fifthParameter)
 */
 data class Genre(
-    var genreName: String?,
-    var startPeriod: LocalDate?,
-    var startCountry: String?,
-    var signature: String?,
-    var bpmRange: Pair<Int, Int>?,
-) : DataFile(genreName, startPeriod, startCountry, signature, bpmRange) {
+    var genreName: String,
+    var startPeriod: String,
+    var startCountry: String,
+    var signature: String,
+    var bpmAverage: Double,
+) : DataFile(genreName, startPeriod, startCountry, signature, bpmAverage) {
     override fun setParameter(index: Int,content: Any){
         this.genreName = content.toString()
     }
@@ -50,12 +51,12 @@ data class Genre(
 
 
 data class Band(
-    var bandName: String?,
-    var creationDate: LocalDate?,
-    var genreName: String?,
-    var startLocation: String?,
-    var creatorsName: String?,
-) : DataFile(bandName, genreName, creatorsName, startLocation, creatorsName)
+    var bandName: String,
+    var creationYear: Int,
+    var genre: Genre,
+    var typeOfBand: Char,
+    var analogicalInstruments: Boolean,
+) : DataFile(bandName, genre, creationYear, typeOfBand, analogicalInstruments)
 
 fun saveData(userData: DataFile, file: File) {
     ObjectOutputStream(file.outputStream()).use {
@@ -69,20 +70,62 @@ fun loadData(file: File): DataFile? {
     }
 }
 
+fun loadFiles(directory: String): File? {
+    val directory = File(directory)
+    val files = directory.listFiles()
+    for (file in files) {
+        if (file.isDirectory) {
+            loadFiles(file.absolutePath)
+        } else {
+            println(file.absolutePath)
+            return file
+        }
+    }
+    return null
+}
+
 //CRUD OPERATIONS
 //Create
 fun createFile() {
     println("What do you want to create?")
-    var dataFile = iterateFileOptions()
-    /*for (field in dataFile::class.java.declaredFields) {
-        field.trySetAccessible()
-        println("Insert the \"${field.name}\"")
-    }*/
-    for(index in 0..dataFile::class.java.declaredFields.size-2){
-        println("Insert the \"${dataFile::class.java.declaredFields[index].name}\"")
-        dataFile.setParameter(index, scanner.next())
+    var dataFile: DataFile
+
+    while (true) {
+        showFileOptions()
+        selectedOption = scanner.nextInt()
+        when (selectedOption) {
+            1 -> {
+                var genreName: String = scanner.next()
+                var startPeriod: String = scanner.next()
+                var startCountry: String = scanner.next()
+                var signature: String = scanner.next()
+                var bmpAverage: Double = scanner.nextDouble()
+                dataFile = Genre(genreName, startPeriod, startCountry, signature, bmpAverage)
+                var genreFile: File = File("DataFiles\\$genreName","$genreName.bin")
+                saveData(dataFile, genreFile)
+                println(dataFile)
+                break
+            }
+            2 -> {
+                var bandName: String = scanner.next()
+                var creationYear: Int = scanner.nextInt()
+                var genrePath: String = scanner.next()
+                var file: File? = loadFiles(genrePath)
+                if (file == null) {
+                    println("Insert a valid Genre")
+                    break
+                }
+                var genre: Genre = loadData(file) as Genre
+                var typeOfBand: Char = scanner.next() as Char
+                var analogicalInstruments: Boolean = scanner.nextBoolean()
+                dataFile = Band(bandName, creationYear, genre, typeOfBand, analogicalInstruments)
+                println(dataFile)
+                break
+            }
+        }
     }
-    println(dataFile)
+
+
 }
 
 
@@ -115,21 +158,6 @@ fun showFileOptions() {
     println("2. Band")
 }
 
-fun iterateFileOptions(): DataFile {
-    while (true) {
-        showFileOptions()
-        selectedOption = scanner.nextInt()
-        when (selectedOption) {
-            1 -> {
-                return Genre("", null, null, null, null)
-            }
-            2 -> {
-                return Band(null, null, null, null, null)
-            }
-        }
-    }
-
-}
 
 fun iterateMainMenu() {
     while (true) {
